5b0113efdf60e41fbed2b813d2fab4e988956f10
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-05 16:40:53 +0200

    Update App.tsx

--- django_project/frontend/src/App.tsx
+++ django_project/frontend/src/App.tsx
@@ -1,28 +1,17 @@
-import * as Sentry from "@sentry/react";
-import React from 'react';
-import { createRoot } from "react-dom/client";
+import React from "react";
+import Routes from "./Routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
-import Home from "./Home";
-import reportWebVitals from './reportWebVitals';
-import ErrorBoundary from "./components/ErrorBoundary";
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Routes />
+      </Router>
+    </ChakraProvider>
+  );
+}
 
-import './styles/index.scss';
-
-Sentry.init({
-    dsn: (window as any).sentryDsn,
-    tunnel: '/sentry-proxy/',
-    tracesSampleRate: 0.5
-})
-
-const rootElement = document.getElementById('app')!
-const root = createRoot(rootElement);
-root.render(
-    <ErrorBoundary>
-        <Home/>
-    </ErrorBoundary>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
