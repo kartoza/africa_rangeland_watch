d38cfb675e94820bd7943caad75b4c48c78b3da5
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-05 16:45:08 +0200

    feat: define theme provider

---
+++ django_project/frontend/src/theme/foundations/colors.ts
@@ -0,0 +1,27 @@
+const colors = {
+    dark_green: {
+      "800": "#005c4a",
+    },
+    light_green: {
+      "400": "#a8d159",
+    },
+    orange: {
+      a200: "#f9953f",
+    },
+    dark_orange: {
+      "800": "#ff8d3a"
+    },
+    light_orange: {
+      "400": "#ffc27a"
+    },    
+    white: {
+      a700: "#ffffff",
+    },
+    black: {
+      "600": "#656d76",
+      A700_91: "#000000",
+    },
+    gray_shadow: "#00000029",
+  };
+  
+  export default colors;
