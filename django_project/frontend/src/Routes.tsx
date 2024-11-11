ebf30e01bd39eb8ed43b1679ab1033f17530f06e
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-11 13:29:03 +0200

    Add profile page (#39)
    * patch: add react pro sidebar
    
    * feat: add profile page
    
    * feat: define sidebar component
    
    * feat: add profile route
    
    * patch: make responsive
    
    * patch: improve header
    
    * patch: make page responsive
    
    * feat: create request organisation form
    
    * patch: link request org button to component
    
    * patch: update image path
    
    * patch: add missing icons
    
    * patch: add static path
    
    * patch: add static path
    
    * patch: upload missing icons
    
    * Update email_icon.svg

--- django_project/frontend/src/Routes.tsx
+++ django_project/frontend/src/Routes.tsx
@@ -3,12 +3,14 @@ import { useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import HomePage from "./pages/Home/index";
+import ProfileInformationPage from "./pages/Profile";
 
 const ProjectRoutes = () => {
   const element = useRoutes([
     { path: "", element: <HomePage /> },
     { path: "/routes", element: <Home /> },
-    { path: "/home", element: <HomePage />}
+    { path: "/home", element: <HomePage />},
+    { path: "/profile", element: <ProfileInformationPage />}
   ]);
 
   return element;
