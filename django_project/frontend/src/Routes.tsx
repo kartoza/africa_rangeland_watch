cebd85d90b07b083f1b51edc6dcc5d0512f9ff10
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-12 12:54:59 +0200

    feat: add path

--- django_project/frontend/src/Routes.tsx
+++ django_project/frontend/src/Routes.tsx
@@ -4,13 +4,15 @@ import NotFound from "./pages/NotFound";
 import HomePage from "./pages/Home/index";
 import OrganisationInformation from "./pages/OrganisationInformation";
 import ProfileInformationPage from "./pages/Profile";
+import AnalysisResults from "./pages/AnalysisResults";
 
 const ProjectRoutes = () => {
   const element = useRoutes([
     { path: "*", element: <NotFound /> },
     { path: "/", element: <HomePage />},
     { path: "/organisation", element: <OrganisationInformation /> },
-    { path: "/profile", element: <ProfileInformationPage />}
+    { path: "/profile", element: <ProfileInformationPage />},
+    { path: "/analysis-results", element: <AnalysisResults />},
   ]);
 
   return element;
