28e82abfe2b8390ba8d6abd5e5c6c43eb4de2540
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-12 02:42:24 +0200

    feat: check if user is authenticated

---
+++ django_project/core/custom_auth_view.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+class CheckTokenView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user 
+        return Response({
+            "message": "Token is valid",
+            "user": {
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name
+            }
+        }, status=status.HTTP_200_OK)
+
