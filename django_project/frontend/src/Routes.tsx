230390c78f50c53532ebce6546fc26ffe16d7143
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-12 02:29:58 +0200

    feat: define protected routes

--- django_project/frontend/src/Routes.tsx
+++ django_project/frontend/src/Routes.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 import HomePage from "./pages/Home/index";
+import NotFound from "./pages/NotFound";
 import ProfileInformationPage from "./pages/Profile";
+import OrganisationInformation from "./pages/OrganisationInformation";
+import PrivateRoute from "./PrivateRoute";
 
 const ProjectRoutes = () => {
-  const element = useRoutes([
-    { path: "", element: <HomePage /> },
-    { path: "/routes", element: <Home /> },
-    { path: "/home", element: <HomePage />},
-    { path: "/profile", element: <ProfileInformationPage />}
+  const routes = useRoutes([
+    { path: "/", element: <HomePage /> },
+    { path: "*", element: <NotFound /> },
+
+    // Use PrivateRoute for protected routes
+    {
+      path: "/profile",
+      element: <PrivateRoute Component={ProfileInformationPage} />,
+    },
+    {
+      path: "/organisation",
+      element: <PrivateRoute Component={OrganisationInformation} />,
+    },
   ]);
 
-  return element;
+  return routes;
 };
 
 export default ProjectRoutes;
