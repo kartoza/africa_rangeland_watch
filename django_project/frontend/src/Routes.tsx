37ab18fe6faee670ac3b55686bd0189cc6ceec98
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-12 02:14:03 +0200

    patch: update routes

--- django_project/frontend/src/Routes.tsx
+++ django_project/frontend/src/Routes.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import HomePage from "./pages/Home/index";
 import OrganisationInformation from "./pages/OrganisationInformation";
@@ -8,8 +7,6 @@ import OrganisationInformation from "./pages/OrganisationInformation";
 const ProjectRoutes = () => {
   const element = useRoutes([
     { path: "*", element: <NotFound /> },
-    { path: "/routes", element: <Home /> },
-    { path: "/home", element: <HomePage />},
     { path: "/", element: <HomePage />},
     { path: "/organisation", element: <OrganisationInformation /> }
   ]);
