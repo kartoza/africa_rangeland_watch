eff6c3e4061e9297fe7a005ee48b2477b53576b4
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-12 02:29:14 +0200

    feat: protect routes
    for start it will retrieve from localstorage but when i improve it will make api request to check if user is still authenticated then allow to proceed to route

---
+++ django_project/frontend/src/PrivateRoute.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+interface PrivateRouteProps {
+  Component: React.ComponentType;
+}
+
+const PrivateRoute = ({ Component }: PrivateRouteProps) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated) {
+    return <Component />;
+  }
+
+  return <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
