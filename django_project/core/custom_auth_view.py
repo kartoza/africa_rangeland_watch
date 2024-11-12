9bcabf04230e32dc3fdb3415189665a8c00ce4b2
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-12 02:55:22 +0200

    patch: fix flake errors

--- django_project/core/custom_auth_view.py
+++ django_project/core/custom_auth_view.py
@@ -3,11 +3,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class CheckTokenView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        user = request.user 
+        user = request.user
         return Response({
             "message": "Token is valid",
             "user": {
@@ -16,4 +17,3 @@ class CheckTokenView(APIView):
                 "last_name": user.last_name
             }
         }, status=status.HTTP_200_OK)
-
