83e487d2a9bf1d9251183cf0b9f2101fd69f2c1a
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-16 16:21:20 +0200

    patch: make menu dynamic

--- django_project/frontend/src/components/MegaMenu/index.tsx
+++ django_project/frontend/src/components/MegaMenu/index.tsx
@@ -1,77 +1,116 @@
 import { Box, Text, Link, Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { logoutUser } from "../../store/authSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store";
 
-export default function MegaMenu() {
+type MenuItem = {
+  label: string;
+  to: string;
+  onClick?: () => void;
+};
+
+type MegaMenuProps = {
+  hoveredSection: string | null;
+  isUserAvatarHovered: boolean;
+};
+
+export default function MegaMenu({ hoveredSection, isUserAvatarHovered }: MegaMenuProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    navigate('/');
+  };
+
+  // Menu items based on hover
+  const menuItems: Record<string, MenuItem[]> = {
+    about: [
+      { label: "Africa RangeWatch", to: "/africa-rangewatch" },
+      { label: "Conversation", to: "/conversation" },
+      { label: "Learn More", to: "/learn-more" },
+    ],
+    userAvatar: !isAuthenticated
+      ? [{ label: "Login", to: "/login" }]
+      : [
+          { label: "Admin", to: "/profile" },
+          { label: "Logout", to: "", onClick: handleLogout }
+      ],
+    resources: [
+      { label: "ARW Documentation", to: "/documentation" },
+      { label: "Resources", to: "/resources" },
+    ],
+  };
+
   return (
-    <Box position="absolute" top="auto" minW="200px" pt="12px" zIndex={99}>
-      <Box bg="whiteAlpha.700" boxShadow="xs" w="100%" p="20px" borderRadius="8px">
+    <Box
+      position={"absolute"}
+      top="auto"
+      pt="12px"
+      zIndex={99}
+      left={{base: isUserAvatarHovered ? "80%" : undefined, md: isUserAvatarHovered? "94%" : undefined}}
+    >
+      <Box bg="whiteAlpha.900" boxShadow="xs" w="100%" p="20px" borderRadius="8px">
         <Flex gap="30px">
-          <Flex gap="16px" flexDirection="column" alignItems="start">
-            <Text color="blackAlpha.700" fontSize={{ base: "15px", sm: "18px" }} fontWeight={700}>
-              Title 1
-            </Text>
-            <Flex gap="12px" flexDirection="column" alignItems="start">
-              <Link href="#" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 1</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 2</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 3</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 4</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 5</Text>
-              </Link>
+          {/* About Section */}
+          {hoveredSection === "about" && (
+            <Flex gap="16px" flexDirection="column" alignItems="start">
+              <Text color="black" fontSize={{ base: "15px", sm: "18px" }} fontWeight={700}>
+                About
+              </Text>
+              <Flex gap="12px" flexDirection="column" alignItems="start">
+                {menuItems.about.map((item) => (
+                  <Link as={RouterLink} to={item.to} key={item.label} fontSize={{ base: "13px", sm: "medium" }}>
+                    <Text color="black" fontSize="16px" fontWeight={400}>{item.label}</Text>
+                  </Link>
+                ))}
+              </Flex>
             </Flex>
-          </Flex>
-          <Flex gap="16px" flexDirection="column" alignItems="start">
-            <Text color="blackAlpha.700" fontSize={{ base: "15px", sm: "18px" }} fontWeight={700}>
-              Title 2
-            </Text>
-            <Flex gap="12px" flexDirection="column" alignItems="start">
-              <Link href="#" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 1</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 2</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 3</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 4</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 5</Text>
-              </Link>
+          )}
+
+          {/* User Avatar Section */}
+          {hoveredSection === "userAvatar" && (
+            <Flex gap="16px" flexDirection="column" alignItems="start">
+              <Text color="black" fontSize={{ base: "15px", sm: "18px" }} fontWeight={700}>
+                User
+              </Text>
+              <Flex gap="12px" flexDirection="column" alignItems="start">
+                {menuItems.userAvatar.map((item) => (
+                  <Link as={RouterLink} to={item.to} key={item.label} fontSize={{ base: "13px", sm: "medium" }} onClick={item.onClick}>
+                    <Text color="black" fontSize="16px" fontWeight={400}>{item.label}</Text>
+                  </Link>
+                ))}
+              </Flex>
             </Flex>
-          </Flex>
-          <Flex gap="16px" flexDirection="column" alignItems="start">
-            <Text color="blackAlpha.700" fontSize={{ base: "15px", sm: "18px" }} fontWeight={700}>
-              Title 3
-            </Text>
-            <Flex gap="12px" flexDirection="column" alignItems="start">
-              <Link href="#" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 1</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 2</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 3</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 4</Text>
-              </Link>
-              <Link href="#" alignSelf="center" fontSize={{ base: "13px", sm: "medium" }}>
-                <Text color="black.600" fontSize="16px" fontWeight={400}>Menu 5</Text>
-              </Link>
+          )}
+
+          {/* Resources Section */}
+          {hoveredSection === "resources" && (
+            <Flex gap="16px" flexDirection="column" alignItems="start">
+              <Text color="black" fontSize={{ base: "15px", sm: "18px" }} fontWeight={700}>
+                Resources
+              </Text>
+              <Flex gap="12px" flexDirection="column" alignItems="start">
+                {menuItems.resources.map((item) => (
+                  <Link as={RouterLink} to={item.to} key={item.label} fontSize={{ base: "13px", sm: "medium" }}>
+                    <Text color="black" fontSize="16px" fontWeight={400}>{item.label}</Text>
+                  </Link>
+                ))}
+              </Flex>
             </Flex>
-          </Flex>
+          )}
         </Flex>
       </Box>
     </Box>
