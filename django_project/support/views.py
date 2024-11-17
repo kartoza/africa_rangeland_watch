903934c9456da3a3684bc1023921aafcd50f0fa7
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-17 13:22:58 +0200

    Update views.py

--- django_project/support/views.py
+++ django_project/support/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
-from .models import Ticket,IssueType
+from .models import Ticket, IssueType
 from .serializers import TicketSerializer, IssueTypeSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
