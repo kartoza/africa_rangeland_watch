3680826e3ad4e6d43219acdfb3677591a76a75e4
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-17 12:31:07 +0200

    patch: fix flake 8

--- django_project/support/serializers.py
+++ django_project/support/serializers.py
@@ -10,19 +10,25 @@ class IssueTypeSerializer(serializers.ModelSerializer):
         model = IssueType
         fields = ['id', 'name']
 
+
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fields = ['id', 'title', 'issue_type', 'description', 'email', 'file_attachment', 'status', 'created_at', 'updated_at']
+        fields = [
+            'id', 'title', 'issue_type', 'description', 'email',
+            'file_attachment', 'status', 'created_at', 'updated_at'
+        ]
 
-    # If you want to allow updating the attachment after ticket creation, this will handle the file properly
     def validate_file_attachment(self, value):
         if value and not value.name.endswith(('.jpg', '.png', '.pdf')):
-            raise serializers.ValidationError("File format not supported. Only JPG, PNG, and PDF are allowed.")
+            raise serializers.ValidationError(
+                "File format not supported. Only JPG, PNG, and PDF are"
+                "allowed."
+            )
         return value
+
 
 class TicketCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'email', 'file_attachment']
-
