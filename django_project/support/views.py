396bbb772c2a7b656fd2b04c2889cf2506608727
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-19 15:38:37 +0200

    Update views.py

--- django_project/support/views.py
+++ django_project/support/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from .models import Ticket, IssueType
 from .serializers import TicketSerializer, IssueTypeSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.exceptions import ValidationError
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -21,7 +21,7 @@ class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes = [IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
 
     def get_queryset(self):
         if self.request.user.is_staff:
@@ -63,14 +63,30 @@ class TicketViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['patch'])
     def update_status(self, request, pk=None):
         """Update the ticket status."""
-
         ticket = self.get_object()
-        status = request.data.get('status', None)
+        status_value = request.data.get('status', None)
 
-        if status and status in dict(Ticket.STATUS_CHOICES):
-            ticket.status = status
+        if status_value == 'resolved' and not request.data.get(
+            'resolution_summary'
+        ):
+            return Response(
+                {"error": "A resolution summary is required when marking the"
+                    " ticket as resolved."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Update status if valid
+        if status_value and status_value in dict(Ticket.STATUS_CHOICES):
+            ticket.status = status_value
+
+            # If status is 'resolved', set the resolution summary
+            if status_value == 'resolved':
+                ticket.resolution_summary = request.data.get(
+                    'resolution_summary', '')
+
             ticket.save()
             ticket.send_status_update_email()
+
             return Response(
                 {"status": "Ticket status updated."},
                 status=status.HTTP_200_OK
@@ -80,6 +96,8 @@ class TicketViewSet(viewsets.ModelViewSet):
             {"error": "Invalid status"},
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
 
     @action(detail=True, methods=['post'])
     def associate_alert(self, request, pk=None):
