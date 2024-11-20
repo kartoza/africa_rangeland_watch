6e790f280d64557342c2f6fa1b17861d4886c683
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-20 03:22:56 +0200

    Update custom_auth_view.py

--- django_project/core/custom_auth_view.py
+++ django_project/core/custom_auth_view.py
@@ -16,6 +16,8 @@ from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from django.core.exceptions import MultipleObjectsReturned
+
 
 
 class CheckTokenView(APIView):
@@ -114,5 +116,87 @@ class AccountActivationView(APIView):
 
         return JsonResponse(
             {'error': 'Invalid activation link'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class ForgotPasswordView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+
+        try:
+            users = get_user_model().objects.filter(email=email)
+            if not users.exists():
+                return Response(
+                    {'error': 'Email not found'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            elif users.count() > 1:
+                return Response(
+                    {'error': 'Multiple users with this email address'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            user = users.first()
+
+        except MultipleObjectsReturned:
+            return Response(
+                {'error': 'Multiple users with this email address'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(str(user.pk).encode())
+
+        reset_password_link = (
+            f"{get_current_site(request).domain}/auth/password-reset/"
+            f"{uid}/{token}/"
+        )
+
+        # Send the password reset email
+        subject = "Password Reset"
+        message = render_to_string('account/password_reset_email.html', {
+            'user': user,
+            'reset_password_url': reset_password_link,
+            'django_backend_url': settings.DJANGO_BACKEND_URL,
+        })
+        send_mail(subject, message, settings.NO_REPLY_EMAIL, [email])
+
+        return Response(
+            {'message': 'Password reset link sent to your email.'},
+            status=status.HTTP_200_OK
+        )
+
+
+
+class ResetPasswordConfirmView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = get_user_model().objects.get(pk=uid)
+        except (
+            TypeError, ValueError,
+            OverflowError,
+            get_user_model().DoesNotExist
+        ):
+            return Response(
+                {'error': 'Invalid reset link'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if default_token_generator.check_token(user, token):
+            new_password = request.data.get('new_password')
+            user.set_password(new_password)
+            user.save()
+            return Response(
+                {'message': 'Password has been successfully reset.'},
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            {'error': 'Invalid reset link'},
             status=status.HTTP_400_BAD_REQUEST
         )
