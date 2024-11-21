e3e146e5648ae5c6e8c1c9d640682963fcabba94
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-21 10:40:56 +0200

    feat: define view for fetching organisation data

--- django_project/base/views.py
+++ django_project/base/views.py
@@ -2,7 +2,35 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import OrganisationInviteForm
 from .models import Organisation
+from django.http import JsonResponse
 
+
+@login_required
+def fetch_organisation_data(request):
+    """
+    Fetch organisation data based on the user's role and type.
+    Organisation managers can view all members and invitations.
+    Organisation members can only view members.
+    """
+    user_profile = request.user.profile
+
+    if not user_profile.organisation:
+        return JsonResponse(
+            {"error": "User is not part of an organisation."},
+            status=403
+        )
+
+    organisation = user_profile.organisation
+    members = organisation.members.all().values("user__username", "user_role")
+    invitations = []
+
+    if user_profile.user_type == "organisation_manager":
+        invitations = organisation.invitations.all().values("email", "status")
+
+    return JsonResponse({
+        "members": list(members),
+        "invitations": list(invitations),
+    }, safe=False)
 
 @login_required
 def invite_to_organisation(request, organisation_id):
