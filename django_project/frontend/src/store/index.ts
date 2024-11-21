c653e3d1d76215ad0f04597399ab81fcf3143652
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-21 10:59:21 +0200

    Update index.ts

--- django_project/frontend/src/store/index.ts
+++ django_project/frontend/src/store/index.ts
@@ -7,7 +7,7 @@ const store = configureStore({
   reducer: {
     auth: authReducer,
     ticket: ticketReducer,
-    organisation: organisationReducer
+    organization: organizationReducer
   },
 });
 
