0706b003a046217e5eeacab3ae69b7b959147a88
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-21 20:52:24 +0200

    Update forms.py

--- django_project/base/forms.py
+++ django_project/base/forms.py
@@ -4,20 +4,27 @@ from invitations.utils import get_invitation_model
 Invitation = get_invitation_model()
 
 
+
 class OrganisationInviteForm(forms.Form):
     email = forms.EmailField()
 
-    def send_invitation(self, request, inviter, organisation):
-        """Send invitation to the email address."""
+    def send_invitation(self, request, inviter, organisation, message):
+        """Send invitation with a custom email template."""
         email = self.cleaned_data['email']
         invitation = (
             Invitation.objects.filter(
                 email__iexact=email
             ).order_by('created').last())
+
         if invitation is None:
             invitation = Invitation.create(
                 email=email,
                 inviter=inviter,
                 organisation=organisation
             )
-            invitation.send_invitation(request=request)
+
+            # Send the invitation with a custom template
+            invitation.send_invitation(
+                request=request,
+                custom_message=message
+            )
