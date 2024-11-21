a565148905b4ffe48c2822599a562c6a2f0ffa09
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-21 10:38:42 +0200

    feat: export oganisation store

--- django_project/frontend/src/store/index.ts
+++ django_project/frontend/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import ticketReducer from './ticketSlice';
+import organisationReducer from './organisationSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     ticket: ticketReducer,
+    organisation: organisationReducer
   },
 });
 
