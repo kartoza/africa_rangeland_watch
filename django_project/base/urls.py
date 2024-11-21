b03d6306e2077493445e8bc35d7480e2f2c7f2ab
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-21 23:27:35 +0200

    Update urls.py

--- django_project/base/urls.py
+++ django_project/base/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('organisation/<int:organisation_id>/',
          views.organisation_detail,
          name='organisation_detail'),
-    path('organisation/<int:organisation_id>/invite/',
+    path('api/organisation/<int:organisation_id>/invite/',
          views.invite_to_organisation,
          name='invite_to_organization'),
     path('api/organizations',
