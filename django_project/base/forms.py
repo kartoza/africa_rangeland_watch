a994a3b9ad61e66d3ca17fb796f0be930dc2cbd3
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-22 08:39:57 +0200

    Update forms.py

--- django_project/base/forms.py
+++ django_project/base/forms.py
@@ -4,8 +4,15 @@ from invitations.utils import get_invitation_model
 Invitation = get_invitation_model()
 
 
+class JSONCompatibleForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        # Check if JSON data is passed
+        if isinstance(args[0], dict):
+            args = (args[0],)
+        super().__init__(*args, **kwargs)
 
-class OrganisationInviteForm(forms.Form):
+
+class OrganisationInviteForm(JSONCompatibleForm):
     email = forms.EmailField()
 
     def send_invitation(self, request, inviter, organisation, message):
@@ -16,15 +23,15 @@ class OrganisationInviteForm(forms.Form):
                 email__iexact=email
             ).order_by('created').last())
 
+
         if invitation is None:
             invitation = Invitation.create(
                 email=email,
                 inviter=inviter,
                 organisation=organisation
             )
-
-            # Send the invitation with a custom template
             invitation.send_invitation(
                 request=request,
                 custom_message=message
             )
+
