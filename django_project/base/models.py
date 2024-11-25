193d3b975cdb81b29a0a6579a3ebff7d6774ca7e
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-25 16:43:49 +0200

    Update models.py

--- django_project/base/models.py
+++ django_project/base/models.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from invitations.models import Invitation
-
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.urls import reverse
+import uuid
 
 class Organisation(models.Model):
     """
