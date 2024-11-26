e512cae0184bff7646019a0f410057a97178dbc4
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-26 11:45:03 +0200

    Update PrivateRoute.tsx

--- django_project/frontend/src/PrivateRoute.tsx
+++ django_project/frontend/src/PrivateRoute.tsx
@@ -1,62 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
-import SignIn from "./components/SignIn";
+import { useLocation, useNavigate } from "react-router-dom";
 import { selectIsLoggedIn } from "./store/authSlice";
 
 interface PrivateRouteProps {
   Component: React.ComponentType;
 }
 
-interface LocationState {
-  from: {
-    pathname: string;
-  };
-}
-
 
 const PrivateRoute = ({ Component }: PrivateRouteProps) => {
   const isAuthenticated = useSelector(selectIsLoggedIn);
-  const [isSignInOpen, setIsSignInOpen] = useState(false);
   const location = useLocation();
   const token = localStorage.getItem("auth_token");
   const navigate = useNavigate();
 
-  const fromLocation = sessionStorage.getItem("redirectBack") || '/';
-
-
-  const handleLoginSuccess = () => {
-    setIsSignInOpen(false);
-    return <Component />;
-  };
-
-  
-
   useEffect(() => {
-    if (isAuthenticated) {
-      handleLoginSuccess();
-    } else {
-      setIsSignInOpen(true);
+    if (!isAuthenticated) {
+      const attemptedPath = location.pathname + location.search;
+      sessionStorage.setItem("redirectAfterLogin", attemptedPath);
+
+      navigate("/")
+
     }
-  }, [isAuthenticated, location.pathname]);
+  }, [isAuthenticated, location.pathname, location.search]);
 
   if (isAuthenticated || token) {
     return <Component />;
   }
 
-  return (
-    <>
-      {isSignInOpen && (
-        <SignIn
-          isOpen={isSignInOpen}
-          onClose={() => {
-            setIsSignInOpen(false);
-            navigate(fromLocation);
-          }}
-        />
-      )}
-    </>
-  );
+ 
 };
 
 export default PrivateRoute;
