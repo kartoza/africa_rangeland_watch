de591e48edbc6c996488782808ff9f06ed9b70e0
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-11-26 10:19:02 +0200

    Update dev.py

--- django_project/core/settings/dev.py
+++ django_project/core/settings/dev.py
@@ -14,6 +14,16 @@ ALLOWED_HOSTS = ['*']
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'core.resend_email_backend.ResendBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp')
+# Port for sending e-mail.
+EMAIL_PORT = ast.literal_eval(os.environ.get('EMAIL_PORT', '25'))
+# SMTP authentication information for EMAIL_HOST.
+# See fig.yml for where these are defined
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'noreply@kartoza.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'docker')
+EMAIL_USE_TLS = ast.literal_eval(os.environ.get('EMAIL_USE_TLS', 'False'))
+EMAIL_USE_SSL = ast.literal_eval(os.environ.get('EMAIL_USE_SSL', 'False'))
+EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX', '')
 
 # Make sure static files storage is set to default
 STATIC_FILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
