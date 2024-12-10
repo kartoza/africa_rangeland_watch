7040a522ce1bba7fa883ed20d0bc4f6815985d98
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-12-10 16:21:00 +0200

    Update urls.py

--- django_project/base/urls.py
+++ django_project/base/urls.py
@@ -1,40 +1,40 @@
 from django.urls import path
-from . import views, user_profile_views
+from . import organisation_views, user_profile_views
 
 
 urlpatterns = [
     path('organisation/<int:organisation_id>/',
-         views.organisation_detail,
+         organisation_views.organisation_detail,
          name='organisation_detail'),
     path('api/organizations',
-         views.fetch_organisation_data,
+         organisation_views.fetch_organisation_data,
          name='fetch_organisation_data'),
     path(
         'api/organizations/member/delete/',
-        views.delete_organisation_member,
+        organisation_views.delete_organisation_member,
         name='delete_member'),
     path(
         'api/organization/<int:organisation_id>/invite/',
-        views.invite_to_organisation,
+        organisation_views.invite_to_organisation,
         name='invite_to_organisation'),
     path(
         'organisation/invite/accept/<int:invitation_id>/',
-        views.accept_invite,
+        organisation_views.accept_invite,
         name='organisation-invite-accept'
     ),
     path(
         'api/join-organization/',
-        views.join_organisation,
+        organisation_views.join_organisation,
         name='join_organization'
     ),
     path(
         'api/add-organization/',
-        views.add_organisation,
+        organisation_views.add_organisation,
         name='add_organization'
     ),
     path(
         'api/fetch-organizations/',
-        views.fetch_organisations,
+        organisation_views.fetch_organisations,
         name='fetch_organization'
     ),
     path(
