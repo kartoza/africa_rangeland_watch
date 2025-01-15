2645a5946b6eecbeaed0d30b7ed27d0996ded88b
tinashe <tinashec@saya-setona.co.za>
2025-01-15 09:43:17 +0200

    save analysis activities

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -26,7 +26,6 @@ from core.views.custom_auth_view import (
     ResetPasswordConfirmView,
     user_info,
     logout_view,
-    CustomLoginView
 )
 from core.views.session_view import UserSessionViewSet
 
@@ -47,7 +46,6 @@ urlpatterns = [
     path('api/logout/', logout_view, name='logout'),
     path('accounts/', include('allauth.urls')),
     path('auth/', include('dj_rest_auth.urls')),
-    path('auth/login/', CustomLoginView.as_view(), name='custom_login'),
     path('auth/activation/', include('allauth.account.urls')),
     path(
         'api/auth/check-token/',
