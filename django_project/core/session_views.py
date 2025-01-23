e2d219ef32832a5b55904e95e2dfd50c91519cba
Dimas <dimas@kartoza.com>
2025-01-23 14:16:32 +0000

    Fix multiple sessions per user

--- django_project/core/session_views.py
+++ django_project/core/session_views.py
@@ -8,20 +8,26 @@ from rest_framework.permissions import IsAuthenticated
 class UserSessionViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
+    def get_user_session(self, request):
+        try:
+            session, created = UserSession.objects.get_or_create(
+                user=request.user
+            )
+        except UserSession.MultipleObjectsReturned:
+            session = UserSession.objects.filter(
+                user=request.user).last()
+            UserSession.objects.exclude(
+                id=session.id
+            ).delete()
+        return session
+
     def retrieve(self, request):
-        session, created = UserSession.objects.get_or_create(
-            user=request.user
-        )
-        serializer = UserSessionSerializer(session)
+        serializer = UserSessionSerializer(self.get_user_session(request))
         return Response(serializer.data)
 
     def update(self, request):
-        session, created = UserSession.objects.get_or_create(
-            user=request.user
-        )
-
+        session = self.get_user_session(request)
         data = request.data
-
         if 'analysisState' in data:
             session.analysis_state = data.get('analysisState')
 
