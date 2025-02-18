3c2ae807148553af2aeff17df46c703e941eedbe
tinashe <tinashec@saya-setona.co.za>
2025-02-18 08:19:27 +0200

    use draggable for card swaps

--- django_project/frontend/src/pages/Dashboard/index.tsx
+++ django_project/frontend/src/pages/Dashboard/index.tsx
@@ -250,7 +250,7 @@ const DashboardPage: React.FC = () => {
                   {(provided) => (
                     <Grid
                       templateColumns="repeat(3, 1fr)"
-                      gap={2}
+                      gap={3}
                       ref={provided.innerRef}
                       {...provided.droppableProps}
                     >
@@ -308,7 +308,7 @@ const DashboardPage: React.FC = () => {
                       ref={provided.innerRef}
                       {...provided.droppableProps}
                       style={{ display: "flex", flexDirection: "column", overflowY: "auto" }}
-                      gap={2}
+                      gap={3}
                     >
                       {balancedPanels.map((config, index) => {
                         const isDummy = config === null;
