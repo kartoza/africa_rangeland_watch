18caa651cb710f725a2815a27166fa71545e283a
Osundwa Jeff <mulamajeff@tuta.io>
2025-04-15 10:33:25 +0300

    Testing (#431)
    * Update Playwright tests and configuration for dev environment
    
    * Tests: Add tests on dev
    
    * Update Playwright configuration and tests for CI environment
    
    * Update CI to use upload-artifact@v4
    
    * Update curl command in CI workflow to use localhost:61100
    
    * Update playwright tests and config to use localhost for CI
    
    * Fix failing CI tests - playwright
    
    * Add CI tests on uploads and downloads #441
    
    * fix failing and flaky tests

---
+++ playwright/ci-test/tests/2-dashboard.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+let url = '/';
+
+test.use({
+  storageState: 'auth.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto(url);
+  await expect(page.locator('h1')).toContainText('Africa Rangeland Watch');
+  await expect(page.locator('a').filter({ hasText: 'DASHBOARD' })).toBeVisible();
+  await page.locator('a').filter({ hasText: 'DASHBOARD' }).click();
+  await expect(page.locator('#app')).toContainText('0 resources found');
+  await expect(page.getByRole('button', { name: 'Filter' })).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Search resources...' })).toBeEmpty();
+  await expect(page.getByRole('button', { name: 'New' })).toBeVisible();
+  await expect(page.getByLabel('Layout Settings')).toBeVisible();
+  await page.getByText('Filter0 resources foundNewOrganise byLayoutHorizontalVerticalNested').click();
+  await page.getByRole('img', { name: 'Footer Logo' }).click();
+  await expect(page.getByRole('heading', { name: 'HELP' })).toBeVisible();
+  await page.getByText('Filter0 resources foundNewOrganise byLayoutHorizontalVerticalNested').click();
+  await page.getByRole('link', { name: 'Visit Support Page' }).click();
+  await page.getByText('No data available').click();
+  await page.locator('a').filter({ hasText: 'My Dashboard' }).click();
+});
---
+++ playwright/ci-test/tests/2-map.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+let url = '/';
+
+test.use({
+  storageState: 'auth.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto(url);
+  await expect(page.locator('h1')).toContainText('Africa Rangeland Watch');
+  await expect(page.getByRole('link', { name: 'MAP' })).toBeVisible();
+  await page.getByRole('link', { name: 'MAP' }).click();
+  await expect(page.getByRole('region', { name: 'Map' })).toBeVisible();
+  await expect(page.getByRole('tab', { name: 'Layers' })).toBeVisible();
+  await expect(page.getByRole('tab', { name: 'Analysis' })).toBeVisible();
+  await expect(page.getByRole('button', { name: ') Select landscape' })).toBeVisible();
+  await page.getByRole('combobox').selectOption('Drakensberg Sub-Escarpment');
+  await page.getByRole('combobox').selectOption('Bahine NP');
+  await page.getByText('Temporal').click();
+  await page.getByText('Baseline', { exact: true }).click();
+  await expect(page.getByRole('textbox', { name: 'Start Date' })).toBeEmpty();
+  await expect(page.getByRole('textbox', { name: 'End Date' })).toBeEmpty();
+  await expect(page.getByText('Click polygons on the map')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Reset Form' })).toBeVisible();
+  await page.getByRole('button', { name: 'Zoom out' }).click();
+  await page.getByRole('button', { name: 'Zoom out' }).click();
+  await page.getByRole('button', { name: 'Zoom in' }).click();
+  await page.getByRole('button', { name: 'Zoom in' }).click();
+  await page.getByRole('button', { name: 'Reset bearing to north' }).click();
+  await page.locator('.BasemapSelector').click();
+  await page.locator('.BasemapSelector').click();
+  await page.locator('.BasemapSelector').click();
+  await expect(page.getByRole('link', { name: 'MapLibre' })).toBeVisible();
+  await page.getByLabel('Toggle attribution').click();
+  await page.getByLabel('Toggle attribution').click();
+  await expect(page.getByRole('link', { name: 'MapLibre' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Upload' })).toBeVisible();
+  await expect(page.locator('.css-uylvmb > .chakra-icon')).toBeVisible();
+});
