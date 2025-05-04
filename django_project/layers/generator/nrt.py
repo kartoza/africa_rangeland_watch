8ff36e1e7d9956dd5124fcb5bec2663a376e86c5
Jeff Osundwa <jeff@kartoza.com>
2025-05-04 22:37:05 +0300

    Revert "Near-real-time data should be available for all key indicators"
    This reverts commit cfe1c88c088d17ab2818ed6720130a73dc78466d.

--- django_project/layers/generator/nrt.py
+++ django_project/layers/generator/nrt.py
@@ -2,15 +2,16 @@
 """
 Africa Rangeland Watch (ARW).
 
-.. note:: Layer Generator for Near-Real Time Layers with PMTiles support.
+.. note:: Layer Generator for Near-Real Time Layers.
 """
 import logging
 import ee
 
-from analysis.models import Landscape
-from analysis.analysis import get_nrt_sentinel
+from analysis.models import Landscape, GEEAsset
+from analysis.analysis import get_nrt_sentinel, train_bgt, classify_bgt
 from layers.models import InputLayer
 from layers.generator.base import BaseLayerGenerator, LayerCacheResult
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,105 +26,83 @@ class NearRealTimeGenerator(BaseLayerGenerator):
         polygon_coords = list(landscape.bbox.coords[0])
         return ee.Geometry.Polygon(polygon_coords)
 
-    def _pmtiles_url(self, layer_name: str, landscape_id: str) -> str:
-        """Generate PMTiles URL for a given layer and landscape."""
-        safe_name = layer_name.lower().replace(' ', '_')
-        return f'/media/pmtiles/{safe_name}_{landscape_id}.pmtiles'
-
     def _generate_evi_layer(self, nrt_img, landscape: Landscape):
+        """Generate EVI layer for a landscape."""
         try:
+            evi_img = nrt_img.select('evi')
+
             evi_layer = InputLayer.objects.get(
                 name='EVI',
                 data_provider=self.get_provider(),
                 group__name='near-real-time'
             )
+
             return LayerCacheResult(
                 evi_layer,
-                self._pmtiles_url('evi', landscape.id),
-                str(landscape.id)
+                evi_img.getMapId(
+                    self.metadata_to_vis_params(evi_layer)
+                )['tile_fetcher'].url_format,
+                f'{landscape.id}'
             )
         except Exception as ex:
-            logger.error(f'_generate_evi_layer failed on {landscape}')
+            logger.error(
+                f'_generate_evi_layer is failed on {landscape}'
+            )
             logger.error(ex)
             return None
 
     def _generate_ndvi_layer(self, nrt_img, landscape: Landscape):
+        """Generate NDVI layer for a landscape."""
         try:
+            ndvi_img = nrt_img.select('ndvi')
+
             ndvi_layer = InputLayer.objects.get(
                 name='NDVI',
                 data_provider=self.get_provider(),
                 group__name='near-real-time'
             )
+
             return LayerCacheResult(
                 ndvi_layer,
-                self._pmtiles_url('ndvi', landscape.id),
-                str(landscape.id)
+                ndvi_img.getMapId(
+                    self.metadata_to_vis_params(ndvi_layer)
+                )['tile_fetcher'].url_format,
+                f'{landscape.id}'
             )
         except Exception as ex:
-            logger.error(f'_generate_ndvi_layer failed on {landscape}')
+            logger.error(
+                f'_generate_ndvi_layer is failed on {landscape}'
+            )
             logger.error(ex)
             return None
 
     def _generate_bare_ground_layer(self, nrt_img, aoi, landscape: Landscape):
+        """Generate bare ground layer for a landscape."""
+        # train and classify bare ground
         try:
-            InputLayer.objects.get(
+            classifier = train_bgt(
+                aoi,
+                GEEAsset.fetch_asset_source('random_forest_training')
+            )
+            bg = classify_bgt(nrt_img, classifier).select('bare')
+
+            bg_layer = InputLayer.objects.get(
                 name='Bare ground cover',
                 data_provider=self.get_provider(),
                 group__name='near-real-time'
             )
-            return LayerCacheResult(
-                InputLayer.objects.get(
-                    name='Bare ground cover',
-                    data_provider=self.get_provider(),
-                    group__name='near-real-time'
-                ),
-                self._pmtiles_url('bare_ground_cover', landscape.id),
-                str(landscape.id)
-            )
-        except Exception as ex:
-            logger.error(f'_generate_bare_ground_layer failed on {landscape}')
-            logger.error(ex)
-            return None
 
-    def _generate_grass_layer(self, nrt_img, aoi, landscape: Landscape):
-        try:
-            InputLayer.objects.get(
-                name='Grass cover',
-                data_provider=self.get_provider(),
-                group__name='near-real-time'
-            )
             return LayerCacheResult(
-                InputLayer.objects.get(
-                    name='Grass cover',
-                    data_provider=self.get_provider(),
-                    group__name='near-real-time'
-                ),
-                self._pmtiles_url('grass_cover', landscape.id),
-                str(landscape.id)
+                bg_layer,
+                bg.getMapId(
+                    self.metadata_to_vis_params(bg_layer)
+                )['tile_fetcher'].url_format,
+                f'{landscape.id}'
             )
         except Exception as ex:
-            logger.error(f'_generate_grass_layer failed on {landscape}')
-            logger.error(ex)
-            return None
-
-    def _generate_woody_layer(self, nrt_img, aoi, landscape: Landscape):
-        try:
-            InputLayer.objects.get(
-                name='Woody plant cover',
-                data_provider=self.get_provider(),
-                group__name='near-real-time'
+            logger.error(
+                f'_generate_bare_ground_layer is failed on {landscape}'
             )
-            return LayerCacheResult(
-                InputLayer.objects.get(
-                    name='Woody plant cover',
-                    data_provider=self.get_provider(),
-                    group__name='near-real-time'
-                ),
-                self._pmtiles_url('woody_plant_cover', landscape.id),
-                str(landscape.id)
-            )
-        except Exception as ex:
-            logger.error(f'_generate_woody_layer failed on {landscape}')
             logger.error(ex)
             return None
 
@@ -133,26 +112,18 @@ class NearRealTimeGenerator(BaseLayerGenerator):
 
         for landscape in Landscape.objects.all().iterator(chunk_size=1):
             aoi = self._to_ee_polygon(landscape)
-            nrt_img = get_nrt_sentinel(aoi, self.DEFAULT_MONTHS)
-
+            nrt_img = get_nrt_sentinel(
+                aoi,
+                self.DEFAULT_MONTHS
+            )
             evi = self._generate_evi_layer(nrt_img, landscape)
             if evi:
                 results.append(evi)
-
             ndvi = self._generate_ndvi_layer(nrt_img, landscape)
             if ndvi:
                 results.append(ndvi)
-
             bg = self._generate_bare_ground_layer(nrt_img, aoi, landscape)
             if bg:
                 results.append(bg)
-
-            grass = self._generate_grass_layer(nrt_img, aoi, landscape)
-            if grass:
-                results.append(grass)
-
-            woody = self._generate_woody_layer(nrt_img, aoi, landscape)
-            if woody:
-                results.append(woody)
 
         return results
