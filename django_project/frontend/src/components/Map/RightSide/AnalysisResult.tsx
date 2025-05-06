94dc5d7ac186ed4b9959b54d7f262d479d60e680
Danang Massandy <danangmassandy@gmail.com>
2025-05-06 10:25:30 +0100

    add download as pdf

--- django_project/frontend/src/components/Map/RightSide/AnalysisResult.tsx
+++ django_project/frontend/src/components/Map/RightSide/AnalysisResult.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Box, Center, Spinner, Table, Text } from "@chakra-ui/react";
+import React, { useRef } from 'react';
+import { Box, Center, Spinner, Table, Text, Flex, IconButton } from "@chakra-ui/react";
+import { FiDownload } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store";
 import { Analysis } from "../../../store/analysisSlice";
 import { Bar, Line } from "react-chartjs-2";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import {FeatureCollection} from "geojson";
 import 'chartjs-adapter-date-fns';
 import { getTrendLineData, formatMonthYear } from "../../../utils/chartUtils";
@@ -16,8 +19,10 @@ Chart.register(CategoryScale);
 
 interface Props {
   analysis: Analysis;
+  decimalPlaces?: number;
 }
 
+const DEFAULT_DECIMAL_PLACES = 3;
 const COLORS = [
   "#FF0000", // Red
   "#0000FF", // Blue
@@ -48,9 +53,10 @@ const splitAndTruncateString = (str: string, maxLength: number) => {
 }
 
 
-export function StatisticTable({analysis}: Props) {
+export function StatisticTable({analysis, decimalPlaces}: Props) {
   const statistics = analysis.results[0].statistics;
   const variable = analysis.data.variable;
+  const _decimalPlaces = decimalPlaces || DEFAULT_DECIMAL_PLACES;
 
   const renderRows = () => {
     const rows: any[] = [];
@@ -61,9 +67,9 @@ export function StatisticTable({analysis}: Props) {
           <tr key={`${year}-${area}`}>
             <td>{year}</td>
             <td>{area}</td>
-            <td>{data.min !== null ? data.min.toFixed(3) : 'N/A'}</td>
-            <td>{data.max !== null ? data.max.toFixed(3) : 'N/A'}</td>
-            <td>{data.mean !== null ? data.mean.toFixed(3) : 'N/A'}</td>
+            <td>{data.min !== null ? data.min.toFixed(_decimalPlaces) : 'N/A'}</td>
+            <td>{data.max !== null ? data.max.toFixed(_decimalPlaces) : 'N/A'}</td>
+            <td>{data.mean !== null ? data.mean.toFixed(_decimalPlaces) : 'N/A'}</td>
           </tr>
         );
       });
@@ -315,13 +321,28 @@ function SpatialBarChart({ analysis }: Props) {
 }
 
 
-export function RenderBaseline({ analysis }: Props) {
-  const keys = Object.keys(analysis.results.columns)
-  return <Box maxWidth={400} overflowX={"auto"}>
+export function RenderBaseline({ analysis, decimalPlaces }: Props) {
+  const _decimalPlaces = decimalPlaces || DEFAULT_DECIMAL_PLACES;
+  const excludeColumns: string[] = [
+    'system:index',
+    'Project'
+  ];
+  let keys: string[] = ['Name'];
+  const _keys = Object.keys(analysis.results.columns);
+  for (let i = 0; i < _keys.length; i++) {
+    const key = _keys[i];
+    if (keys.includes(key)) {
+      continue;
+    }
+    if (excludeColumns.includes(key)) {
+      continue;
+    }
+    keys.push(key);
+  }
+  return <Box id="BaselineTableContainer" maxWidth={400} overflowX={"auto"}>
     <Table className='BaselineAnalysisResultTable' cellPadding={8}>
       <thead>
       <tr>
-        <th>Name</th>
         {
           keys.map(
             (column: string) => <th key={column}>{column}</th>
@@ -334,11 +355,10 @@ export function RenderBaseline({ analysis }: Props) {
         analysis.results.features.map((feature: any, index: any) => {
           const properties = feature.properties;
           return <tr key={index}>
-            <td>{properties.Name}</td>
             {
               keys.map(
                 (column: string) => <td key={column}>
-                  {properties[column]}
+                  {typeof properties[column] === 'number' ? properties[column].toFixed(_decimalPlaces) : properties[column]}
                 </td>
               )
             }
@@ -386,18 +406,126 @@ export default function AnalysisResult() {
     error,
     analysis
   } = useSelector((state: RootState) => state.analysis);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const downloadPDF = async () => {
+    if (!containerRef.current) return;
+  
+    // Hide the dashboard name and icons temporarily
+    const iconsElement = document.getElementById("download-button");
+  
+    if (iconsElement) iconsElement.style.display = "none";
+
+    // remove box shadow
+    containerRef.current.style.boxShadow = "none";
+
+    const pdfOrientation = analysis.data.analysisType === "Baseline" ? "l" : "p";
+  
+    // if Baseline, we need to expand the table
+    let originalScrollLeft = null;
+    let originalWidth = null;
+    if (analysis.data.analysisType === "Baseline") {
+      const table = document.getElementById("BaselineTableContainer");
+      if (table) {
+        originalScrollLeft = table.scrollLeft;
+        originalWidth = table.style.width;
+
+        // Expand to full scrollable width
+        table.style.width = table.scrollWidth + 'px';
+
+        console.log('table.scrollWidth ',table.scrollWidth)
+      }
+    }
+
+    const canvas = await html2canvas(containerRef.current, {
+      backgroundColor: "white"
+    });
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF(pdfOrientation, "mm", "a4");
+  
+    // Restore the hidden elements
+    if (iconsElement) iconsElement.style.display = "block";
+
+    // Restore box shadow
+    containerRef.current.style.boxShadow = "0px 0px 5px 0px #00000030";
+  
+    // Restore the table width
+    if (analysis.data.analysisType === "Baseline") {
+      const table = document.getElementById("BaselineTableContainer");
+      if (table) {
+        table.style.width = originalWidth;
+        table.scrollLeft = originalScrollLeft;
+      }
+    }
+
+    console.log(canvas.width, canvas.height)
+    const imgWidth = 200;
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+  
+    // Add the chart image
+    pdf.addImage(imgData, "PNG", 10, 10, imgWidth, imgHeight);
+  
+    let fileName = '';
+    if (analysis.data.analysisType === "Baseline") {
+      fileName = 'Baseline Analysis';
+    } else if (analysis.data.analysisType === "Temporal") {
+      fileName = `${analysis.data.temporalResolution} Temporal Analysis on ${analysis.data.variable}`;
+    } else if (analysis.data.analysisType === "Spatial") {
+      fileName = `Spatial Analysis on ${analysis.data.variable}`;
+    }
+
+    pdf.save(`${fileName}.pdf`);
+  }
+
   if (!loading && !error && !analysis) {
     return null
   }
+
+  let header = null;
+  if (analysis?.data.analysisType === "Baseline") {
+    header = 'Baseline Analysis'
+    if (analysis?.data.baselineStartDate && analysis.data.baselineEndDate) {
+      let startDate = new Date(analysis.data.baselineStartDate);
+      let endDate = new Date(analysis.data.baselineEndDate);
+      const currentLocale = navigator.language || 'en-US';
+      header += ` (${startDate.toLocaleDateString(currentLocale, { day: 'numeric', month: 'long', year: 'numeric' })} - ${endDate.toLocaleDateString(currentLocale, { day: 'numeric', month: 'long', year: 'numeric' })})`
+    }
+  } else if (analysis?.data.analysisType === "Temporal") {
+    header = `${analysis.data.temporalResolution} Temporal Analysis on ${analysis.data.variable}`
+  } else if (analysis?.data.analysisType === "Spatial") {
+    header = `Spatial Analysis on ${analysis.data.variable}`
+    if (analysis.data.spatialStartYear && analysis.data.spatialEndYear) {
+      header += ` (${analysis.data.spatialStartYear} - ${analysis.data.spatialEndYear})`
+    }
+  }
+
   return (
-    <Box backgroundColor='white'
+    <Box ref={containerRef} backgroundColor='white'
          borderRadius={8}
          boxShadow="0px 0px 5px 0px #00000030"
          pointerEvents='auto'
          p={4}>
-      <Text fontSize="1.5rem" fontWeight={600} color='green.600'>
-        Statistics
-      </Text>
+      <Flex width="100%" align="center" justify="space-between">
+        <Text fontSize="1.5rem" fontWeight={600} color='green.600'>
+          Statistics
+        </Text>
+        <IconButton
+          id="download-button"
+          icon={<FiDownload />} 
+          onClick={downloadPDF} 
+          colorScheme="teal" 
+          aria-label="Download"
+          size="sm" 
+        />
+      </Flex>
+      {header && (
+        <Box id="analysis-header" marginTop={2} marginBottom={2}>
+          <Text fontSize="1.0rem" fontWeight={600} color='green.600'>
+            {header}
+          </Text>
+        </Box>
+      )}
+      
       {
         loading ?
           <Box>
