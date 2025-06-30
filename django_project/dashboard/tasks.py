0e701629387aa025c8a513ce3cbad52abf291657
Z.A. Muzakki <zakki@kartoza.com>
2025-06-30 00:44:38 +0000

    generate thumbnail

---
+++ django_project/dashboard/tasks.py
@@ -0,0 +1,11 @@
+from core.celery import app
+from dashboard.models import Dashboard
+
+
+@app.task(name='generate_dashboard_thumbnail')
+def generate_dashboard_thumbnails(dashboard_ids):
+    """Generate thumbnails for a dashboard."""
+    from dashboard.models import Dashboard
+    dashboards = Dashboard.objects.filter(uuid__in=dashboard_ids)
+    for dashboard in dashboards:
+        dashboard.generate_and_save_thumbnail()
