41dd4e3f9d90df9bd46d6a50961ef786d34fbb83
Z.A. Muzakki <zakki@kartoza.com>
2026-04-23 04:50:50 +0000

    Fix lint

--- django_project/frontend/api_views/earth_ranger_events.py
+++ django_project/frontend/api_views/earth_ranger_events.py
@@ -11,7 +11,7 @@ from django.db import connection
 from django.db.models import Count, Q
 from django.http import Http404, HttpResponse
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import mixins, GenericViewSet
